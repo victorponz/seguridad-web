3fed70df8fdbe2e150153db13b59273e98787855
Víctor Ponz Artero <35145182+victorponz@users.noreply.github.com>
2022-03-29 16:50:01 +0200

    Update post_mejorado.php

--- victima/public_html/post_mejorado.php
+++ victima/public_html/post_mejorado.php
@@ -9,7 +9,7 @@
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
 ?>
 <p>Nuevo Post</p>
-<form action='post.php' method='post'>
+<form action='post_mejorado.php' method='post'>
 	<textarea name="textarea" rows="10" cols="50">Escribe algo aquí</textarea>
 	<input type = 'submit' value='enviar'>
 </form>
